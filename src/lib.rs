7e0499eb8a7bb9afddaf1fd75faf87b69f0a6fbc
shreevari <shreevari.sp@protonmail.com>
2018-12-20 14:33:19 +0530

    Arguments parsed(barely) and Automata works

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,84 @@
-use std::env;
-use std::collections::HashMap;
+extern crate csv;
+use std::error::Error;
+use std::fmt::Debug;
 
 #[derive(Debug)]
-enum OptKind {
-	Short,
-	Long,
-}
-
-#[derive(Debug)]
-enum Opt {
-	Param { name: String, kind: OptKind, value: String },
-	Flag { name: String, kind: OptKind, value: bool }, 
-}
-
-#[derive(Debug)]
-struct Args<'a> {
-	raw_args: Vec<String>,
-	options: Vec<Opt>,
-}
-
-impl Opt {
-	fn new (name: String, kind: OptKind, value: Option<>) -> Result<Opt, &'static str> {
-
-	}
-}
-
-impl Args {
-
-	fn new(args: Vec<String>, options: Vec<Opt>) -> Result<Args, &'static str> {
-		
-		let raw_args: Vec<String> = Vec::new();
-		let mut items = args.iter().enumerate();
-		
-		raw_args.push(items.next().1.clone());
-
-		for (i, item) in items {
-			match item {
-				long if long.starts_with("--") => {
-					let option = raw_args.remove(i);
-					find_option(option, options);
-				},
-				short if short.starts_with("-") => {
-
-				},
-				_ => {
-					raw_args.push(item);
-				},
-			}
-		}
-
-	}
-}
-
-#[derive(Debug)]
-struct Config {
+pub struct Config {
 	input: String,
-	start_state: Opt,
-	final_states: Opt,
-	transition_table: Opt,
-	interactive: bool,
+	states: Vec<u32>,
+	inputs: Vec<char>,
+	transition_table: Vec<Vec<u32>>,
+	start_state: u32,
+	final_states: Vec<u32>,
 }
 
 impl Config {
-	fn new (args: Vec<String>) -> Config {
-		let input = String::new();
-		let start_state = String::new();
-		let final_states;
-		let transition_table;
-		let interactive = false;
+	pub fn new (args: Vec<String>) -> Result<Config, Box<Error>> {
 
-		let mut items = args.iter().enumerate().skip(1);
-		for (i, item) in items {
-			match item {
-				"-i" => {
-					interactive = true;
-				},
-				"-f" => {
-					let filename  = args[i+1];
-
-				}
+		let input = args[1].to_string();
+		let mut inputs = Vec::new();
+		let mut states = Vec::new();
+		let start_state: u32 = args[2].parse()?;
+		let mut final_states = Vec::new(); 
+		let final_states_path = args[4].to_string();
+		let mut transition_table: Vec<Vec<u32>> = Vec::new();
+		let transition_table_path = args[3].to_string();
+		
+		//Read final_states from csv file
+		let mut final_states_reader = csv::ReaderBuilder::new()
+			.has_headers(false)
+			.from_path(final_states_path)?;
+		for record in final_states_reader.records() {
+			for final_state in record?.iter() {
+				final_states.push(final_state.trim().parse()?);
 			}
 		}
+		//println!("final_states : {:?}", final_states);
+
+		let mut transition_table_reader = csv::ReaderBuilder::new()
+			.has_headers(true)
+			.from_path(transition_table_path)?;
+
+		//Read inputs from transition_table csv file
+		for field in transition_table_reader.headers()?.iter().skip(1) {
+			let input: char = field.trim().parse()?;
+			inputs.push(input)
+		}
+		//println!("inputs : {:?}", inputs);
+
+		//Read states and transition_table from transition_table csv file
+		for record in transition_table_reader.records() {
+			let mut row = Vec::new();
+			for delta in record?.iter() {
+				row.push(delta.trim().parse()?)
+			}
+			states.push(row.remove(0));
+			transition_table.push(row.clone());
+		}
+
+		//println!("states : {:?}", states);
+		//println!("transition_table : {:?}", transition_table);
+
+		Ok(Config{ input,states, inputs, transition_table, start_state, final_states })
 	}
+}
+
+pub fn run(config: &Config) -> Result<bool, Box<Error>> {
+	let mut present_state: &u32 = &config.start_state;
+	for symbol in config.input.chars() {
+		let input_index = find(symbol, &config.inputs).unwrap_or_else(|| { panic!("Wrong inputs") });
+		let state_index = find(*present_state, &config.states).unwrap_or_else(|| { panic!("Wrong states"); });
+		present_state = config.transition_table.get(state_index).unwrap().get(input_index).unwrap();
+	}
+	Ok(config.final_states.contains(present_state))
+}
+
+fn find<T: PartialEq+Debug>(key: T, vector: &Vec<T>) -> Option<usize> {
+	for (i, element) in vector.iter().enumerate() {
+
+		if key == *element {
+			return Some(i);
+		}
+	}
+	None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use std::env;
+use std::process;
+use fsmulator;
+use fsmulator::Config;
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	println!("{:?}", args);
+	let config = Config::new(args)
+		.unwrap_or_else(|err| {
+			eprintln!("Error parsing arguments : {}", err);
+			process::exit(1);
+		});
+
+	//println!("{:#?}", config);
+	if fsmulator::run(&config).unwrap() {
+		println!("Accepted");
+	} else {
+		println!("Rejected");
+	}
 }
